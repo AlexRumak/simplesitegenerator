15dccf18da1223e793ebda9c42c2c4eb34539941
Alex Rumak <alexanderrumak@gmail.com>
2025-02-23 21:19:37 -0500

    Fix external var

--- src/tests/lib/ctest.h
+++ src/tests/lib/ctest.h
@@ -5,7 +5,7 @@
 
 // Global buffer
 #define BUFFER_SIZE 1024
-char cTest_current_test_name[BUFFER_SIZE];
+extern char cTest_current_test_name[BUFFER_SIZE];
 
 #define RESOURCE_FILE_BUFFER_SIZE 1024
 char cTest_resourceFilesBasePath[RESOURCE_FILE_BUFFER_SIZE];
